788410fdbad09ab81bdb765848fd89564418f521
Sandoleathy <3296446003@qq.com>
2023-10-12 17:46:35 +0800

    menu page activity create

--- app/src/main/java/com/android/starchinese2023/Menu.java
+++ app/src/main/java/com/android/starchinese2023/Menu.java
@@ -1,5 +1,6 @@
 package com.android.starchinese2023;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,11 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        ActionBar bar = getSupportActionBar();
+        //hide the default tool bar
+        if(bar != null){
+            bar.hide();
+        }
     }
 }
