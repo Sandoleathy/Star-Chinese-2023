78d6501393851549e4758071040de69e342c5585
Sandoleathy <3296446003@qq.com>
2023-10-12 20:44:29 +0800

    add intent between load and menu page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.android.starchinese2023"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.android.starchinese2023"
@@ -30,9 +30,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/android/starchinese2023/Menu.java
+++ app/src/main/java/com/android/starchinese2023/Menu.java
@@ -3,9 +3,13 @@ package com.android.starchinese2023;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,28 @@ public class Menu extends AppCompatActivity {
         //hide the default tool bar
         if(bar != null){
             bar.hide();
+        }
+
+        Button start_button = findViewById(R.id.start_button);
+        Button load_button = findViewById(R.id.load_button);
+        Button ending_button = findViewById(R.id.view_ending_button);
+        start_button.setOnClickListener(this);
+        load_button.setOnClickListener(this);
+        ending_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.start_button){
+            Toast.makeText(this,"Start click",Toast.LENGTH_SHORT).show();
+        }
+        else if(view.getId() == R.id.load_button){
+            Intent toLoad = new Intent(this, LoadGame.class);
+            //toLoad.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(toLoad);
+        }
+        else if(view.getId() == R.id.view_ending_button){
+            Toast.makeText(this,"view ending click",Toast.LENGTH_SHORT).show();
         }
     }
 }
